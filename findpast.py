d1ea6acf00a8b8ebaee4bdaa11fbef1104d70b8f
Jingyu Hou <118997149+JinnyH16@users.noreply.github.com>
2023-02-06 22:05:28 +0000

    Update findpast.py

--- findpast.py
+++ findpast.py
@@ -48,3 +48,42 @@ for i in range(199, len(df)):
 
 print(result_H)
 print(result_A)
+
+H_goals = []
+H_avg_goals = []
+H_goal_difference = []
+H_avg_goal_difference = []
+H_yellow_card = []
+H_avg_yellow_card = []
+H_lists = [H_goals, H_avg_goals, H_goal_difference, H_avg_goal_difference, H_yellow_card, H_avg_yellow_card]
+
+for i in range(len(result_H)):
+    if result_H[i] is None:
+        for lst in H_lists:
+            lst.append(None)
+    else:
+        for j, lst in enumerate(H_lists):
+            lst.append(result_H[i][j])
+
+A_goals = []
+A_avg_goals = []
+A_goal_difference = []
+A_avg_goal_difference = []
+A_yellow_card = []
+A_avg_yellow_card = []
+A_lists = [A_goals, A_avg_goals, A_goal_difference, A_avg_goal_difference, A_yellow_card, A_avg_yellow_card]
+
+for i in range(len(result_A)):
+    if result_A[i] is None:
+        for lst in A_lists:
+            lst.append(None)
+    else:
+        for j, lst in enumerate(A_lists):
+            lst.append(result_A[i][j])
+
+df = df.assign(H_goals = H_goals, H_avg_goals = H_avg_goals, H_goal_difference = H_goal_difference,
+               H_avg_goal_difference = H_avg_goal_difference, H_yellow_card = H_yellow_card, H_avg_yellow_card = H_avg_yellow_card)
+df.to_csv('PremData - complete data - edited.csv', index = False)
+df = df.assign(A_goals = A_goals, A_avg_goals = A_avg_goals, A_goal_difference = A_goal_difference,
+               A_avg_goal_difference = A_avg_goal_difference, A_yellow_card = A_yellow_card, A_avg_yellow_card = A_avg_yellow_card)
+df.to_csv('PremData - complete data - edited.csv', index = False)
