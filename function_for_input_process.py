8cbee9d57754b645eabaf86c4fb4762c72fce6b6
Haofan Dong <uceehd1@ucl.ac.uk>
2023-02-07 13:35:11 +0000

    Just some simple functions for input process
    If an NPL is required, a dataset of questions will be included. Otherwise, we can just use the token to generate a response.

---
+++ function_for_input_process.py
@@ -0,0 +1,42 @@
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+from nltk.tokenize import word_tokenize
+
+
+def tokenize_text(input_text):
+    tokens = word_tokenize(input_text)
+    stop_words = set(stopwords.words('english'))
+    tokens = [token.lower() for token in tokens if token.isalpha() and token.lower() not in stop_words]
+    return ' '.join(tokens)
+
+
+def classify_intent(text):
+    if 'hello' in text:
+        return 'greeting'
+    elif 'bye' in text:
+        return 'farewell'
+    else:
+        return 'none'
+
+
+def generate_response(intent):
+    if intent == 'greeting':
+        return 'Hello! How can I help you today?'
+    elif intent == 'farewell':
+        return 'Goodbye! Have a great day.'
+    else:
+        return 'I am not sure what you mean.'
+
+
+def chatbot_response(input_text):
+    processed_text = tokenize_text(input_text)
+    intent = classify_intent(processed_text)
+    response = generate_response(intent)
+    return response
+
+
+print(chatbot_response("HELLO from Haofan"))
