ef2187b2f58ee95a7e490e3a9d4a364e34944263
YuangFeng <zceefen@ucl.ac.uk>
2023-02-15 19:45:05 +0000

    Feature engineering, more features and functions

--- Feature_eng.py
+++ Feature_eng.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('PremData - complete data.csv')#read data
+df = pd.read_csv('allSeasons - PremData - complete data.csv')#read data
 
 def findpast(season, H_name, A_name, find_name, x):#Function return new features of single team in past x matches
     loc = df[(df['Season'] == season) & (df['H'] == H_name) & (df['A'] == A_name)]
@@ -32,6 +32,58 @@ def findpast(season, H_name, A_name, find_name, x):#Function return new features
     if count == 0:
         return None
 
+def findpast_homeonly(season, H_name, A_name, find_name, x):
+    #Function return new features of single team in past x matches, at home only!
+    loc = df[(df['Season'] == season) & (df['H'] == H_name) & (df['A'] == A_name)]
+    if loc.size == 0:
+        return None
+    index = loc.index.tolist()[0]
+    count = 0
+    goals = []
+    goal_difference = []
+    yellow_card = []
+    points = []
+    for i in range(index + 1, len(df)):
+        if df.iloc[i]['H'] == find_name:
+            goals.append(df.iloc[i]['H Goals'])
+            goal_difference.append(df.iloc[i]['H Goals'] - df.iloc[i]['A Goals'])
+            yellow_card.append(df.iloc[i]['H yellows'])
+            points.append(df.iloc[i]['H points'])
+        else:
+            continue
+        count += 1
+        if count == x:
+            return sum(goals) , sum(goals)/x, sum(goal_difference), sum(goal_difference)/x, sum(yellow_card), sum(yellow_card)/x, sum(points)
+            break
+    if count == 0:
+        return None
+
+def findpast_awayonly(season, H_name, A_name, find_name, x):
+    #Function return new features of single team in past x matches, at home only!
+    loc = df[(df['Season'] == season) & (df['H'] == H_name) & (df['A'] == A_name)]
+    if loc.size == 0:
+        return None
+    index = loc.index.tolist()[0]
+    count = 0
+    goals = []
+    goal_difference = []
+    yellow_card = []
+    points = []
+    for i in range(index + 1, len(df)):
+        if df.iloc[i]['A'] == find_name:
+            goals.append(df.iloc[i]['A Goals'])
+            goal_difference.append(df.iloc[i]['A Goals'] - df.iloc[i]['H Goals'])
+            yellow_card.append(df.iloc[i]['A yellows'])
+            points.append(df.iloc[i]['A points'])
+        else:
+            continue
+        count += 1
+        if count == x:
+            return sum(goals) , sum(goals)/x, sum(goal_difference), sum(goal_difference)/x, sum(yellow_card), sum(yellow_card)/x, sum(points)
+            break
+    if count == 0:
+        return None
+
 def meetingrecord(season, H_name, A_name, findname, x):#Function that return  meeting records of past x matches between two teams
     loc = df[(df['Season'] == season) & (df['H'] == H_name) & (df['A'] == A_name)]
     if loc.size == 0:
@@ -53,32 +105,90 @@ def meetingrecord(season, H_name, A_name, findname, x):#Function that return  me
     if count == 0:
         return None
 
+def countstreak(nums):
+
+    res = {-1:0, 0:0, 1:0}
+    l = 1
+    previous = nums[0]
+
+    for i in range(l, len(nums)):
+        if nums[i] == previous:
+            l += 1
+        else:
+            l = 1
+
+        res[nums[i]] = max(res[nums[i]], l)
+        previous = nums[i]
+
+    return res[1], res[0], res[-1] #maximum streaks of wins, draws and losses
+
+def streak(season, H_name, A_name, find_name, x):#return maximum streaks of wins, draws and losses in past 5 matches
+    loc = df[(df['Season'] == season) & (df['H'] == H_name) & (df['A'] == A_name)]
+    if loc.size == 0:
+        return None
+    index = loc.index.tolist()[0]
+    count = 0
+    past = [] ## store win/draw/loss records of past 5 games of one particular team
+    for i in range(index + 1, len(df)):
+        if df.iloc[i]['A'] == find_name:
+            if df.iloc[i]['A Goals'] - df.iloc[i]['H Goals'] > 0:
+                past.append(1) ## 1 stand for win
+            elif df.iloc[i]['A Goals'] - df.iloc[i]['H Goals'] == 0:
+                past.append(0) ## 0 stand for draw
+            else:## -1 stand for draw
+                past.append(-1)
+        elif df.iloc[i]['H'] == find_name:
+            if df.iloc[i]['H Goals'] - df.iloc[i]['A Goals'] > 0:
+                past.append(1) ## 1 stand for win
+            elif df.iloc[i]['H Goals'] - df.iloc[i]['A Goals'] == 0:
+                past.append(0) ## 0 stand for draw
+            else:## -1 stand for draw
+                past.append(-1)
+        else:
+            continue
+        count += 1
+        if count == x:
+            return countstreak(past)
+            break
+    if count == 0:
+        return None
+
 
 result_H_past5 = {}
 result_A_past5 = {}
 result_H_past20 = {}
 result_A_past20 = {}
 Meeting_record_A_3 = {}
-Meeting_record_H_3 ={}## Dictionaries to store features
+Meeting_record_H_3 = {}
+result_H_at_home5 = {}
+result_A_at_away5 = {}
+streak_A_past5 = {}
+streak_H_past5 = {}
+# Dictionaries to store features
 
 season_cutoff = [199, 579, 959, 1219, 1449, 1599, 1979, 2359, 2739, 3119, 3499, 3879, 4259, 4639] # no. of rows which a new season start
 season = 23
 
 for i in range(0, len(df)):# Looping row by row to create new features
-#    if i > 10:
-#       break
+ #   if i > 5:
+#     break
     for j in season_cutoff:
         if i == j:
-            season = season - 1
+            season -= 1
     Hteam = df.iloc[i,2]
     Ateam = df.iloc[i,3]
     result_H_past5[i] = findpast(season, Hteam, Ateam, Hteam, 5)
     result_A_past5[i] = findpast(season, Hteam, Ateam, Ateam, 5)
     result_H_past20[i] = findpast(season, Hteam, Ateam, Hteam, 20)
     result_A_past20[i] = findpast(season, Hteam, Ateam, Ateam, 20)
+    result_H_at_home5[i] = findpast_homeonly(season, Hteam, Ateam, Hteam ,5)
+    result_A_at_away5[i] = findpast_awayonly(season, Hteam, Ateam, Ateam ,5)
     Meeting_record_A_3[i] = meetingrecord(season, Hteam, Ateam, Ateam, 3)
     Meeting_record_H_3[i] = meetingrecord(season, Hteam, Ateam, Hteam, 3)
-    
+    streak_H_past5[i] = streak(season, Hteam, Ateam, Hteam, 5)
+    streak_A_past5[i] = streak(season, Hteam, Ateam, Ateam, 5)
+
+"""
 # Initializing lists for storing dictionary data
 goals = []
 avg_goals = []
@@ -150,3 +260,5 @@ df.to_csv('allSeasons - PremData - complete data - edited.csv', index=False)
 load_meetingrecord(lists2, Meeting_record_H_3)
 df = df.assign(H_goal_difference3 = past_goal_difference, H_avg_goal_difference3 = avg_past_goal_difference)
 df.to_csv('allSeasons - PremData - complete data - edited.csv', index=False)
+
+"""
