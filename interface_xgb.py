cc614b097c5aa7f2944469c45263d57888d34ef7
YuangFeng <zceefen@ucl.ac.uk>
2023-03-05 17:23:37 +0000

    Debug

--- interface_xgb.py
+++ interface_xgb.py
@@ -29,6 +29,8 @@ def predict(season, H_name, A_name):
                    'A_away_goals5', 'H_home_avg_goals5', 'A_away_avg_goals5', \
                    'H_W_streaks', 'H_D_streaks', 'H_L_streaks', 'A_W_streaks', 'A_D_streaks', 'A_L_streaks']].copy()
     loc = df[(df['Season'] == season) & (df['H'] == H_name) & (df['A'] == A_name)]## locate the predict match
+    if loc.size == 0:
+        return None # can not search the match, the team name input or season input is wrong wrong
     x = loc[['H_avg_goals5', 'A_avg_goals5', 'H_goal_difference5', 'A_goal_difference5', 'H_points5','A_points5',\
                   'H_avg_goals20', 'A_avg_goals20', 'H_goal_difference20', 'A_goal_difference20', 'H_points20','A_points20',\
                       'H_goal_difference3', 'A_goal_difference3',\
